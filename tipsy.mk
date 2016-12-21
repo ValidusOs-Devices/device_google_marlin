29c4051183cf48201e337b6488b42441b2f1a2e1
beanstown106 <nbenis106@gmail.com>
2016-12-20 21:43:02 -0500

    common: kill bugreport shortcut service *this can be triggered easily in pocket remove it
    Change-Id: I51d54129b81b3daadcffd1f93ab552ff7338af51

--- tipsy.mk
+++ tipsy.mk
@@ -2,10 +2,6 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit Carbon product configuration
 $(call inherit-product, vendor/tipsy/config/common.mk)
 
